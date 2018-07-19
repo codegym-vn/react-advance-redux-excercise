443804cbf32005c5fbdbbaf7c91f5b8b8b09ce63
waynerooney998877 <waynerooney998877@gmail.com>
2018-07-19 22:45:42 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# AdvancedReduxExcersiceReactNativeTrainnig
+# Advanced Redux Excersice ReactNative Training
+
+# Mục tiêu bài tập
+- Biết cấu trúc thư mục Project theo Redux
+- Hiểu được nhiệm vụ của các thành phần trong Redux
+- Biết cách truyền dữ liệu từ store tới component
+- Hiểu được ý nghĩa các hàm : createStore, dispatch, mapStateToProps, mapDispatchToProps
+
+# Bài tập
+- Viết 1 ứng dụng đơn giản nổi tiếng couter
+- Giao diện có 2 button tăng và giảm, click button tăng, số sẽ tăng lên,
+click button giảm, số sẽ giảm đi
+- Yêu cầu: cấu trúc thư mục theo Redux
+
+# Giao diện như sau
+
+![redux_advanced]()
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    View
+} from 'react-native';
+import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
+import store from './store/store';
+import CounterContainer from './containers/counterContainer';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <View style={styles.container}>
+                    <CounterContainer/>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
